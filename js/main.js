85953b41eab4d44323a600a343770404aed97f5f
tereshok <kwon_wtf@ukr.net>
2018-12-10 10:59:24 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+;(function($) {
+	"use strict";
+
+$(function(){
+	$('.ba-team-slider').slick({
+		dots: true,
+		prevArrow:".slick-prev",
+		nextArrow:".slick-next"
+
+	});
+})
+
+/*	const slider = $('.ba-team-slider');
+	slider.slick();*/
+
+})(jQuery);
